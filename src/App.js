c3b5068abd6e9399444c5d1a5fe0a9e6a6e059e5
yulianaM <yulianamallorga@gmail.com>
2023-07-11 15:11:32 -0300

    add the tailwind library to the project and the meta keywords and author to the index.html file.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function App() {
   return (
-    <h1>My personal portfolio</h1>
+    <div>
+      <h1 className="text-3xl text-end">My personal portfolio</h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>  
+    </div>
+    
   );
 }
 
